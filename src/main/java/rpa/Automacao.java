3482bbabc488c9fa8b350609b50069365d1807ab
DRamos91 <danielmrg@gmail.com>
2023-04-27 20:21:22 -0300

    Projeto finalizado

--- src/main/java/rpa/Automacao.java
+++ src/main/java/rpa/Automacao.java
@@ -1,5 +1,9 @@
 package rpa;
 
+
+import java.util.List;
+import java.util.Scanner;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -12,7 +16,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Automacao {
+public class Automacao{
 	private WebDriver driver;
 
 	@Before
@@ -20,15 +24,20 @@ public class Automacao {
 		// Atualiza e roda com a vers�o mais atualizada do webdriver
 		WebDriverManager.chromedriver().setup();
 	}
-/*	
-	@After
-	public void tearDown() throws Exception {
-		// Comando para fechar o navegador
-		 driver.quit();
-}
-*/
+
 	@Test
-	public void test() throws NoSuchElementException {		
+	public void test() throws NoSuchElementException {	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite a marca do Carro:");
+		String strMarca = sc.nextLine();
+		System.out.println("Digite o modelo do Carro");
+		String strModelo = sc.nextLine();
+		sc.nextLine();
+		System.out.println("Digite o Ano do Carro");
+		String strAno = sc.nextLine();
+		sc.close();
+		
 		// Comando para acessar o site desejado		
 		ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -41,22 +50,43 @@ public class Automacao {
         // Acessa o site da FIPE
         driver.get("https://veiculos.fipe.org.br/");
 
-        // Seleciona o tipo de veículo
-        
+        // Seleciona o tipo de veículo        
         WebElement tipoCarro = driver.findElement(By.xpath("//*[@id=\"front\"]/div[1]/div[2]/ul/li[1]/a"));
         tipoCarro.click();
-        WebElement selectMarcaCarro = driver.findElement(By.xpath("//*[@id=\"selectMarcacarro_chosen\"]/a"));
-        selectMarcaCarro.click();
-        driver.findElement(By.xpath("//*[@id=\\\"selectMarcacarro_chosen\\\"]/a")).sendKeys("Hyundai", Keys.ENTER);
-
-/*
-        // Seleciona a marca do carro
-        driver.findElement(By.xpath("//*[@id=\"principal\"]/ul/li[2]/a")).click();
-        WebElement hyundai = driver.findElement(By.xpath("//*[@id=\"selectMarcacarro\"]"));
-        System.out.println(hyundai);
-        wait.until(ExpectedConditions.elementToBeClickable(hyundai)).click();
-        System.out.println("clicou");
-*/
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"selectMarcacarro_chosen\"]/a"));
+        element.click();
+        driver.findElement(By.xpath("//*[@id=\"selectMarcacarro_chosen\"]/a"));
+        element.click();
+        //Seleciona a marca do carro
+        WebElement element1 = driver.findElement(By.xpath("//*[@id=\"selectMarcacarro_chosen\"]/div/div/input"));
+        element1.sendKeys(strMarca, Keys.ENTER);
+          
+        //Seleciona o modelo do Carro
+        WebElement selectModel = driver.findElement(By.xpath("//*[@id=\"selectAnoModelocarro_chosen\"]/a"));
+        selectModel.click();        
+        WebElement model = driver.findElement(By.xpath("//*[@id=\"selectAnoModelocarro_chosen\"]/div/div/input"));
+        model.sendKeys(strModelo, Keys.ENTER);
         
+        //Seleciona o modelo do Carro
+        WebElement selectAno = driver.findElement(By.xpath("//*[@id=\"selectAnocarro_chosen\"]/a"));
+        selectAno.click();        
+        WebElement ano = driver.findElement(By.xpath("//*[@id=\"selectAnocarro_chosen\"]/div/div/input"));
+        ano.sendKeys(strAno, Keys.ENTER);  
+      
+        
+        WebElement enter = driver.findElement(By.xpath("//*[@id=\"buttonPesquisarcarro\"]"));
+        enter.click();
+        
+        
+        WebElement tabela = driver.findElement(By.tagName("table"));
+        List<WebElement> linhas = tabela.findElements(By.tagName("tr"));
+        for (WebElement linha : linhas) {
+            List<WebElement> colunas = linha.findElements(By.tagName("td"));
+            for (WebElement coluna : colunas) {
+                System.out.print(coluna.getText() + "\t");
+            }
+            System.out.println();
+        }
+        driver.quit();
 	}
 }
